24e4035b70839b895e0e767a17df3dff08c07228
Michael <michael@ftr-ict.com>
2024-09-26 10:48:45 +0200

    DZNPY-0021: Create release 0.4 of dznpy

--- test/unit_tests/support_files/test_sf_ilog.py
+++ test/unit_tests/support_files/test_sf_ilog.py
@@ -100,7 +100,7 @@ def test_create_default_namespaced():
     assert result.namespace == ns_ids_t('Dzn')
     assert result.filename == 'Dzn_ILog.hh'
     assert result.contents == DEFAULT_DZN_ILOG_HH
-    assert result.contents_hash == '06055f38dbc96dbe2453f515b4f9f961'
+    assert result.contents_hash == '2ad251bccf91b3cc5301067a6b1c796c'
     assert 'namespace Dzn {' in result.contents
 
 
