234681fd6176807a5bc295813781d582f1bf784d
Michael <michael@ftr-ict.com>
2024-12-01 13:48:16 +0100

    DZNPY-0027: Prepare 1.0.DEV

--- src/dznpy/dznpy_version.py
+++ src/dznpy/dznpy_version.py
@@ -7,4 +7,4 @@ Copyright (c) 2023-2024 Michael van de Ven <michael@ftr-ict.com>
 This is free software, released under the MIT License. Refer to dznpy/LICENSE.
 '''
 
-VERSION = '0.5.20241126'
+VERSION = '1.0.DEV'
